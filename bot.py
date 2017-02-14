1ac0be3f71eff24f2c0023ef4b2e472c2a3fbe62
Michael Herman <hermanmu@gmail.com>
2017-02-14 09:40:29 -0700

    updated script

--- bot.py
+++ bot.py
@@ -8,15 +8,21 @@ from selenium import webdriver
 
 # globals
 
-try:
-    USERNAME = os.environ['ANCESTRY_USERNAME']
-    PASSWORD = os.environ['ANCESTRY_PASSWORD']
-except KeyError:
-    print('Please set the correct environment variables')
-    sys.exit(1)
+USERNAME = os.getenv('ANCESTRY_USERNAME', None)
+PASSWORD = os.getenv('ANCESTRY_PASSWORD', None)
+FILE = os.getenv('SURNAME_FILE_NAME', 'surnames.txt')
+BASE = 'http://search.ancestry.com/cgi-bin/sse.dll?db=nypl&gss=sfs28_ms_db&new=1&rank=1&msT=1&MS_AdvCB=1&gsln={0}&gsln_x=1&MSAV=2&uidh=quk'
 
-NAMES = ['gates']
-BASE = 'http://search.ancestry.com/cgi-bin/sse.dll?db=nypl&gss=sfs28_ms_db&new=1&rank=1&msT=1&MS_AdvCB=1&gsln={0}&gsln_x=1&MSAV=2&uidh=quk'.format(NAMES[0])
+
+# functions
+
+def get_surnames():
+    data = []
+    with open(FILE) as f:
+        reader = csv.reader(f)
+        for row in reader:
+            data.append(row[0])
+        return data
 
 
 def authenticate():
@@ -35,40 +41,44 @@ def authenticate():
     sleep(5)
     welcome = driver.find_element_by_xpath('//h1[@class="pageTitle"]').text
     if 'Welcome,' in welcome:
-        print('Successfully logged in!')
+        print('successfully logged in')
         sleep(5)
         return driver
     else:
-        print('Something went wrong!')
+        print('something went wrong')
         driver.quit()
         return False
 
 
-def get_search_results(driver):
-    print('getting records...')
+def get_search_results(driver, array):
     driver.get(BASE)
-    records = driver.find_elements_by_css_selector("tr.record")
-    if not len(records):
-        print('No records!')
-        driver.quit()
-        return False
-    else:
-        for row in records:
-            cell = row.find_elements_by_tag_name('td')[5].text
-            if len(cell) > 1:
-                row.find_elements_by_tag_name('td')[0].click()
-                sleep(5)
-                data = []
-                for value in driver.find_elements_by_tag_name('td'):
-                    data.append(value.text)
-                with open('data.csv', 'a') as f:
-                    w = csv.writer(f)
-                    w.writerow(data)
-            # sleep(5)
-            # driver.quit()
+    for surname in array:
+        print('searching passenger list for {0}...'.format(surname))
+        driver.get(BASE.format(surname))
+        sleep(5)
+        records = driver.find_elements_by_css_selector('tr.record')
+        if not len(records):
+            print('no records found for {0}'.format(surname))
+            driver.quit()
+            return False
+        else:
+            for row in records:
+                cell = row.find_elements_by_tag_name('td')[5].text
+                if len(cell) > 1:
+                    row.find_elements_by_tag_name('td')[0].click()
+                    sleep(5)
+                    data = []
+                    for value in driver.find_elements_by_tag_name('td'):
+                        data.append(value.text)
+                    with open('data.csv', 'a') as f:
+                        w = csv.writer(f)
+                        w.writerow(data)
+                        print('...done')
+                        break
 
 
 if __name__ == '__main__':
+    surnames = get_surnames()
     browser = authenticate()
     if browser:
-        get_search_results(browser)
+        get_search_results(browser, surnames)
